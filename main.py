d601e1c4e40a1157ee48a793c351c7321c0980df
MartinWho2 <martin.crettol@gmail.com>
2021-01-21 08:02:10 +0100

    Added a function to display fps

--- main.py
+++ main.py
@@ -6,13 +6,19 @@ clock = pygame.time.Clock()
 win = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 
 game = Game(win)
-
+def display_fps():
+    "Data that will be rendered and blitted in _display"
+    font = pygame.font.SysFont(None ,(50))
+    text = font.render(str(int(clock.get_fps())),True,(255,255,255))
+    win.blit(text,(0,0))
 def main():
 
     while game.run:
         clock.tick(60)
+
         if not game.pause:
             game.update()
+            display_fps()
             pygame.display.flip()
             game.events()
         else:
